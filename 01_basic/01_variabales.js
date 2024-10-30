e4261ba931db09d7f5bd5451d6262ec46b6eea2d
Shaukeen <117082857+Shaukeenalam@users.noreply.github.com>
2024-10-30 19:34:24 +0000

    variabales

---
+++ 01_basic/01_variabales.js
@@ -0,0 +1,6 @@
+const accountId = 14253;
+let accountEmail = "nobi@gmail.com"
+var accountPassword = "sizuka"
+accountCity = "azamgarh"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
