171905d57e0b496fefa4950d73bd430d53ddc0d7
Shaukeen <117082857+Shaukeenalam@users.noreply.github.com>
2024-11-02 05:39:02 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat al JS code as newer version
+
+//alert(3+3) // we are useing nodejs, not browser
+
+console.log (3+3)
+
+// Documentation "tc39.es" "MDN"
+
+let name = "nobeta"
+let age = 12
+let isLoggedIn = false
+
+//  number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) // output undefined
+console.log(typeof null) // output object
+
+
+
+
+
