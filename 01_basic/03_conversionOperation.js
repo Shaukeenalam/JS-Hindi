4cd258a7e4c3fadaf2119ba9945fc9f57f76bfb8
Shaukeen <117082857+Shaukeenalam@users.noreply.github.com>
2024-11-02 06:36:47 +0000

    convesion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+// "33" => esey convert in 33
+// "33abc" => not an nuumber NaN
+// true => 1; false => 0
+
+let inLoggedIn = "1"
+
+let booleanInLoggedIn = Boolean(inLoggedIn)
+
+console.log(booleanInLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "noita" => true 
