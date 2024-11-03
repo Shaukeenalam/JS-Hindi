d29ceac86b493f2943bc3def8bb627ebbc1d8f0b
Shaukeen <117082857+Shaukeenalam@users.noreply.github.com>
2024-11-03 18:04:36 +0000

    update

---
+++ 02_basic/01_array.js
@@ -0,0 +1,21 @@
+//ARRAY
+
+const myArray = [0,1,2,3,4,5,6]
+const myHeors = ["Shaukeen Alam", "Nobita"]
+
+//console.log(myArray[]);
+
+
+// ARRAY METHODS 
+myHeors.push("sizuka")
+myHeors.push("sizuka")
+//myHeors.pop()
+
+//myHeors.unshift(9)
+//myArray.shift()
+
+console.log(Array.isArray("sizuka"));
+console.log(Array.from("sizuka"));
+console.log(myArray.length);
+console.log(myHeors.length);
+ 
